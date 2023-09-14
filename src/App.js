3cc8e0c01f3c7e447bc3ca6aa9b2ea64961adb90
Airton Santos <airton.santos@unifesp.br>
2023-09-14 11:01:17 -0300

    Autenticação de usuarios!

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
+
 import { AuthProvider } from './context/AuthContext';
 import { onAuthStateChanged } from "firebase/auth";
 import { useState, useEffect } from 'react'
-import { useAuthentication } from './hooks/useAuthentication';
+import { useAuthentication } from './hooks/useAuthentication'; 
 
 import Home from './Pages/Home/Home';
 import About from './Pages/About/About';
@@ -12,6 +13,8 @@ import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
 import Login from './Pages/Login/Login';
 import Register from './Pages/Register/Register';
+import CreatePost from './Pages/CreatePost/CreatePost';
+import Dashboard from './Pages/Dashboard/Dashboard';
 
 
 
@@ -19,14 +22,15 @@ function App() {
 
 
   const [user, setUser] = useState(undefined)
-  const auth = useAuthentication()
+  const {auth} = useAuthentication()
 
   const loadingUser = user === undefined
 
 
 
   useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
+
+    onAuthStateChanged(auth,(user) => {
       setUser(user);
     });
   }, [auth]);
@@ -34,9 +38,10 @@ function App() {
   if (loadingUser) {
     return <p>Carregando..</p>;
   }
+  
   return (
     <div className="App">
-      <AuthProvider>
+      <AuthProvider value={{user}}>
         <BrowserRouter>
           <Navbar />
           <div className='container'>
@@ -44,8 +49,12 @@ function App() {
 
               <Route path='/' element={<Home />} />
               <Route path='/about' element={<About />} />
-              <Route path='/login' element={<Login />} />
-              <Route path='/register' element={<Register />} />
+              <Route path='/login' element={!user ? <Login />: <Navigate to='/'/>} />
+              <Route path='/register' element={!user ? <Register />: <Navigate to='/'/>} />
+              <Route path='/posts/create' element={user ? <CreatePost />: <Navigate to='/login'/>} />
+
+              <Route path='/dashboard' element={user ? <Dashboard />: <Navigate to='/login'/>} />
+
 
             </Routes>
           </div>
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,29 +1,49 @@
-import React from 'react'
+import React from 'react';
 import { NavLink } from 'react-router-dom'
 import styles from './Navbar.module.css';
 
+import { useAuthentication } from '../hooks/useAuthentication';
+import { useAuthValue } from '../context/AuthContext';
+
 const Navbar = () => {
+
+  const { user } = useAuthValue();
+
   return (
-   <nav  className={styles.navbar}>
-    <NavLink className={styles.brand}  to='/'>
+    <nav className={styles.navbar}>
+      <NavLink className={styles.brand} to='/'>
         Mini<span>Blog</span>
-    </NavLink>
-    <ul className={styles.links_list}>
-      <li>  <NavLink to='/'>
-       Home
-    </NavLink></li>
-    <li> <NavLink to='/login'>
-        Entrar
-    </NavLink></li>
-    <li> <NavLink to='/register'>
-        Cadastrar
-    </NavLink></li>
-      <li> <NavLink to='/about'>
-        Sobre
-    </NavLink></li>
-     
-    </ul>
-   </nav>
+      </NavLink>
+      <ul className={styles.links_list}>
+        <li>  <NavLink to='/'>
+          Home
+        </NavLink></li>
+        {!user && (
+          <>
+            <li> <NavLink to='/login'>
+              Entrar
+            </NavLink></li>
+            <li> <NavLink to='/register'>
+              Cadastrar
+            </NavLink></li>
+          </>
+        )}
+         {user && (
+          <>
+            <li> <NavLink to='/posts/create'>
+              Novo Post
+            </NavLink></li>
+            <li> <NavLink to='/dashboar'>
+              Dashboard
+            </NavLink></li>
+          </>
+        )}
+        <li> <NavLink to='/about'>
+          Sobre
+        </NavLink></li>
+
+      </ul>
+    </nav>
   )
 }
 
